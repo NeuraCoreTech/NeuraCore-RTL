e02ae8b234bb1f62edf53903b4dfe2988f7c3b58
ArnavYadnopavit <yadnopavitarnav@gmail.com>
2026-07-04 09:47:50 +0530

    PE

--- rtl/V1.0/mac.sv
+++ rtl/V1.0/mac.sv
@@ -12,19 +12,16 @@ module mac#(
     input  logic signed [MULT_WIDTH-1:0]  psumin,
     output logic signed [MULT_WIDTH-1:0]  psumout
 );
-    // psumin is already MULT_WIDTH - no extension needed
     
     // ================================
     // Xilinx Multiply-Adder IP
-    // Latency: A:B-P = 3, C-P = 2 → so buffered up C:P by 1 cycle to get cycle accurate latency of 3 cycles total
+    // Latency: 3 cycles total
     // Output P is already registered inside the IP
     // ================================
     logic signed [MULT_WIDTH-1:0] buffer_psumin;
-    always_ff @(posedge clk)begin
-        if (sclr) begin
-            buffer_psumin<='0;
-        end
-        else if (ce) buffer_psumin <= psumin;
+    always_ff @(posedge clk) begin
+        if (sclr)    buffer_psumin <= '0;
+        else if (ce) buffer_psumin <= psumin;  
     end
     xbip_multadd_8bit mac_ip (
         .CLK      (clk),
@@ -34,7 +31,7 @@ module mac#(
         .B        (filter),
         .C        (buffer_psumin),
         .SUBTRACT (1'b0),
-        .P        (psumout),   // ← direct wire, no extra FF
+        .P        (psumout),
         .PCOUT    ()
     );
 
